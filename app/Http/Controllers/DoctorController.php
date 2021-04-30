66967e272c76bd0c660be8c947a669648a035a95
G14Retro <G14_0309@hotmail.com>
2021-04-29 19:57:30 -0500

    Respaldo

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -22,14 +22,14 @@ class DoctorController extends Controller
         ->where('dispo.horaInicio','>',Carbon::now())
         ->join('disponibilidadhoraria AS dispo','dispo.id_disponibilidad','citas.disponibilidad')
         ->join('personas','personas.id','citas.id_persona')
-        ->select('personas.id AS id','personas.nombre AS nombre_paciente','personas.apellido AS apellido_paciente',
+        ->select('citas.id_cita AS id','personas.nombre AS nombre_paciente','personas.apellido AS apellido_paciente',
         'dispo.horaInicio AS hora_atencion')
         ->orderBy('hora_atencion','desc')
         ->get();
         if (count($appointment)==0) {
             return response()->json(
                 [
-                    'Message' => 'No tiene citas asignadas para tramitar',
+                    'message' => 'No tiene citas asignadas para tramitar',
                 ]
             );
         }else{
@@ -44,7 +44,8 @@ class DoctorController extends Controller
         $request->validate([
             'id_paciente'   => 'required|string'
         ]);
-        $id_paciente = $request->id_paciente;
+        $id_paciente = Appointment::where('id_cita','=',$request->id_paciente)
+        ->value('id_persona');
         $paciente = User::where('personas.id','=',$id_paciente)
         ->join('tipo_documento','tipo_documento.documento','personas.tipo_documento')
         ->select('tipo_documento.nombre_documento AS tipo_documento','personas.numero_documento AS numero_documento',
@@ -69,9 +70,11 @@ class DoctorController extends Controller
             'cirugias'                  => 'required|string',
             'medicamentos'              => 'required|string',
             'otros'                     => 'required|string',
-            'paciente'                  => 'required|string',
+            'cita'                      => 'required|string',
         ]);
-        $medicalhistory = MedicalHistory::where('paciente','=',$request->paciente)
+        $id_paciente = Appointment::where('id_cita','=',$request->cita)
+        ->value('id_persona');
+        $medicalhistory = MedicalHistory::where('paciente','=',$id_paciente)
         ->get();
         if (count($medicalhistory)==0) {
             $medicalhistory = new MedicalHistory([
@@ -81,17 +84,27 @@ class DoctorController extends Controller
                 'cirugias'                  => strtolower($request->cirugias),
                 'medicamentos'              => strtolower($request->medicamentos),
                 'otros'                     => strtolower($request->otros),
-                'paciente'                  => strtolower($request->paciente),
+                'paciente'                  => $id_paciente,
                 ]);
                 $medicalhistory->save();
-                $availability = Appointment::where('id_persona','=',$request->paciente)
-                ->update(['estado'=>4]);
+                $availability = Appointment::where('id_persona','=',$id_paciente)
+                ->where('id_persona','=',$request->cita)
+                ->update(['estado'=>'#28a745']);
             return response()->json([
                 'message' => 'El registro se ha guardado satisfactoriamente'], 201);
         }else {
-            MedicalHistory::where('paciente','=',$request->paciente)->update($request->all());
-            $availability = Appointment::where('id_persona','=',$request->paciente)
-            ->update(['estado'=>4]);
+            MedicalHistory::where('paciente','=',$id_paciente)->update([
+                'alergias'                  => strtolower($request->alergias),
+                'enfermedades'              => strtolower($request->enfermedades),
+                'enfermedades_familiares'   => strtolower($request->enfermedades_familiares),
+                'cirugias'                  => strtolower($request->cirugias),
+                'medicamentos'              => strtolower($request->medicamentos),
+                'otros'                     => strtolower($request->otros),
+                'paciente'                  => $id_paciente,
+            ]);
+            $availability = Appointment::where('id_persona','=',$id_paciente)
+            ->where('id_cita','=',$request->cita)
+            ->update(['estado'=>'#28a745']);
             return response()->json([
                 'message' => 'El registro se ha actualizado satisfactoriamente'], 202);
         }
